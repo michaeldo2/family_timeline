b53b20fd3486e30ae580cb81a8b66bbf48af3198
Michael H Do <michaeldo@berkeley.edu>
2016-12-24 18:41:53 -0800

    view displaying timeline events

--- timeline/urls.py
+++ timeline/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# ex: /timeline/
     url(r'^$', views.index, name='index'),
+    # ex: /timeline/5/
+    url(r'^event/(?P<event_id>[0-9]+)/$', views.event, name='event')
 ]
--- timeline/views.py
+++ timeline/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Event
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    event_list = Event.objects.order_by('-date')[:5]
+    template = loader.get_template('timeline/index.html')
+    context = {
+        'event_list': event_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def event(request, event_id):
+	return HttpResponse("event" + event_id)
