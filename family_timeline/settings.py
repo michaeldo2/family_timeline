62524a32c5ce420df391e4074e3d7660a08ae07d
Michael H Do <michaeldo@berkeley.edu>
2016-12-25 13:51:03 -0800

    dsads

--- family_timeline/settings.py
+++ family_timeline/settings.py
@@ -133,9 +133,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATIC_URL = os.path.join(BASE_DIR, 'family_timeline/static/')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
