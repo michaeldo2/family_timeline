2d48620e10b7b2cf8c09d5349f88f56b91d4151d
Michael H Do <michaeldo@berkeley.edu>
2016-12-25 13:43:22 -0800

    move static file location

--- family_timeline/settings.py
+++ family_timeline/settings.py
@@ -131,7 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'family_timeline/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = os.path.join(BASE_DIR, 'family_timeline/static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
