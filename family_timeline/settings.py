84c88bf9cfafa8c6e9e0397cbb1518fa3d09be60
Michael H Do <michaeldo@berkeley.edu>
2016-12-25 13:16:04 -0800

    change staticfiles location

--- family_timeline/settings.py
+++ family_timeline/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(PROJECT_PATH, '/static/')
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static/')
 
 
 
