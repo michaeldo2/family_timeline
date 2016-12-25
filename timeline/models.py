6056efbecc1cc6ec35b37c11b59696db5039c2b2
Kelvin Do <kelvin@pinterest.com>
2016-12-24 20:40:40 -0800

    Changed models to use stories

--- timeline/models.py
+++ timeline/models.py
@@ -4,9 +4,23 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Event(models.Model):
-	name = models.CharField(max_length=50)
+	name = models.CharField(max_length=100)
 	pub_date = models.DateTimeField()
-	date = models.DateTimeField()
-	description = models.CharField(max_length=400)
+	date = models.DateField()
+	description = models.TextField()
 	image = models.ImageField()
 	publisher = models.ForeignKey(User, related_name="events_published")
+
+
+class Story(models.Model):
+	event = models.ForeignKey(Event, related_name="stories")
+	publisher = models.ForeignKey(User, related_name="stories")
+	description = models.TextField()
+	pub_date = models.DateTimeField()
+
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, related_name="comments")
+	event = models.ForeignKey(Event, related_name="comments")
+	pub_date = models.DateTimeField()
+	comment = models.TextField()
