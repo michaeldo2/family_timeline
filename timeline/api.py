678b4e3b72179fcf6f17d6399f2b81d4d03ecd73
Kelvin Do <kelvin@pinterest.com>
2016-12-25 16:23:23 -0800

    Simplified the API

--- timeline/api.py
+++ timeline/api.py
@@ -5,4 +5,5 @@ from . import handlers
 urlpatterns = [
 	# ex: /events/
     url(r'^events/$', handlers.events),
+    url(r'^event/(?P<event_id>[0-9]+)$', handlers.event),
 ]
--- timeline/handlers.py
+++ timeline/handlers.py
@@ -2,14 +2,46 @@ import json
 
 from django.http import HttpResponse
 from django.core import serializers
+from django.utils.encoding import force_text
 
 from .models import Comment
 from .models import Event
 from .models import Story
 
+# Constants
+POST = 'POST'
+GET = 'GET'
+
 # Create your api handlers here.
 
 def events(request):
-	events = serializers.serialize("json", Event.objects.all())
-	return HttpResponse(events, content_type="application/json")
+    if request.method == GET:
+        event_objs = Event.objects.all()
+        events = []
+        for event_obj in event_objs:
+            event = {}
 
+            # Event Fields to be Returned
+            event['id'] = event_obj.id
+            event['name'] = event_obj.name
+            event['description'] = event_obj.description
+            event['date'] = event_obj.date.isoformat()
+            event['num_stories'] = len(event_obj.stories.all())
+
+            events.append(event)
+
+        data = json.dumps(events)
+        return HttpResponse(data, content_type="application/json")
+
+    else:
+        raise ValueError('Unsupported HTTP Request Method')
+
+
+def event(request, event_id):
+    if request.method == GET:
+        event = Event.objects.filter(pk=event_id)
+        data = serializers.serialize("json", event)
+        return HttpResponse(data, content_type="application/json")
+
+    else:
+        raise ValueError('Unsupported HTTP Request Method')
