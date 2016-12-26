299317b7da1ef7de4030f517fb2c5457a5ad3a94
Kelvin Do <kelvin@pinterest.com>
2016-12-25 19:00:08 -0800

    Changed event model to use year/index'

--- timeline/models.py
+++ timeline/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,9 +10,11 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Event(models.Model):
 	name = models.CharField(max_length=100)
-	pub_date = models.DateTimeField()
-	date = models.DateField()
-	description = models.TextField()
+	pub_date = models.DateTimeField(default=datetime.now, blank=True)
+	year = models.IntegerField()
+	date = models.DateField(null=True, blank=True)
+	index = models.IntegerField()
+	description = models.TextField(null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
 	publisher = models.ForeignKey(User, related_name="events_published")
 
@@ -38,4 +42,4 @@ class Comment(models.Model):
 	comment = models.TextField()
 
 	def __str__(self):
-		return self.event.name + " by " + self.publisher.username
+		return self.event.name + " by " + publisher.username
