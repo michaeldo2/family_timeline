92d4b3d8bf795b014cca4c846482facbe3b4a6ec
Kelvin Do <kelvin@pinterest.com>
2016-12-26 14:35:19 -0800

    set allowed host to family-timeline

--- family_timeline/settings.py
+++ family_timeline/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'c#zge(n4r8vq48i@z@b-69npxdl510$l(nohn6ebk_ye=#*ymt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['radiant-woodland-59631.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['family_timeline.herokuapp.com', 'localhost']
 
 
 # Application definition
