7cdc69e8eed711882354360531a7861ff72f3b85
Kelvin Do <kelvin@pinterest.com>
2016-12-28 02:16:37 -0800

    Added icons to timeline entrys

--- react/components/Event.jsx
+++ react/components/Event.jsx
@@ -1,18 +1,26 @@
 import React from "react"
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import Group from 'material-ui/svg-icons/social/group';
+import Globe from 'material-ui/svg-icons/social/public';
+import LocalLibrary from 'material-ui/svg-icons/maps/local-library';
+import Avatar from 'material-ui/Avatar';
 
 class Event extends React.Component {
 
     render() {
 
     	var color;
+    	var Icon;
 
     	if (this.props.entry_type === 'FAMILY_EVENT') {
-    		color = '#BBDEFB'
+    		color = '#BBDEFB';
+    		Icon = Group;
     	} else if (this.props.entry_type === 'HISTORICAL_EVENT') {
-    		color = '#FFCDD2'
+    		color = '#FFCDD2';
+    		Icon = Globe;
     	} else if (this.props.entry_type === 'TIMELINE_STORY') {
-    		color = 'FFF9C4'
+    		color = 'FFF9C4';
+    		Icon = LocalLibrary;
     	}
 
     	var style = {
@@ -27,6 +35,11 @@ class Event extends React.Component {
 					actAsExpander={true}
 					showExpandableButton={true}
 					style={style}
+					avatar={
+						<Avatar 
+							icon={<Icon />}
+						/>
+					}
 			    />
 			    <CardText expandable={true}>
 			      	{this.props.description}
